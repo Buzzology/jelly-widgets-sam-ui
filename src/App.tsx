eb1374aa90ecd8f61652babe1d03d7dda40e8905
Buzzology <chrisowens321@gmail.com>
2022-01-19 19:52:28 +1000

    [ADD] Add routes and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import './App.css';
-import AustralianCompanyNumberGenerator from './components/widgets/AustralianCompanyNumberGenerator';
-import AustralianCompanyNumberValidator from './components/widgets/AustralianCompanyNumberValidator';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { EnumWidgetId } from './@types/Widget';
+import ViewHome from './views/generic/ViewHome';
+import ViewAustralianCompanyNumberGenerator from './views/widgets/ViewAustralianCompanyNumberGenerator';
+import ViewAustralianCompanyNumberValidator from './views/widgets/ViewAustralianCompanyNumberValidator';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <AustralianCompanyNumberGenerator />
-        <AustralianCompanyNumberValidator />
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<ViewHome />} />
+        {/* <Route path="widgets" element={<Teams />}> */}
+        <Route path="widgets">
+          <Route path={`${EnumWidgetId.AUSTRALIAN_COMPANY_NUMBER_GENERATOR}/*`} element={<ViewAustralianCompanyNumberGenerator />} />
+          <Route path={`${EnumWidgetId.AUSTRALIAN_COMPANY_NUMBER_VALIDATOR}/*`} element={<ViewAustralianCompanyNumberValidator />} />
+          {/* <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} /> */}
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
