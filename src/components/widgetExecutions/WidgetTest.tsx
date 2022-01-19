71f4a52877c5090559dedf2bda0b5eca3e9fe4d1
Buzzology <chrisowens321@gmail.com>
2022-01-19 18:56:15 +1000

    [ADD] Generate and validate now working

--- src/components/widgetExecutions/WidgetTest.tsx
+++ src/components/widgetExecutions/WidgetTest.tsx
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { WidgetIds } from '../../@types/Widget';
+import { EnumWidgetId } from '../../@types/Widget';
 import { useAppDispatch } from '../../store/hooks';
 import { RootState } from '../../store/root';
 import { v4 as uuidv4 } from 'uuid';
 
-// import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import {
     executeBatchAsync, selectResponseItemsByBatchId,
 } from './widgetExecutionsSlice';
+import { IGenericExecuteBatchRequestItemPayload } from '../../@types/WidgetExecuteBatch';
 
 export function WidgetTest() {
-    //   const count = useAppSelector(selectCount);
     const dispatch = useAppDispatch();
     const [currentBatchId, setCurrentBatchId] = useState("");
     const [batchIdHistory, setBatchIdHistory] = useState<string[]>([]);
+    const [currentInputValue, setCurrentInputValue] = useState<string>("");
     const [loading, setLoading] = useState(false);
     const batchItems = useSelector((state: RootState) => {
         return selectResponseItemsByBatchId(state, currentBatchId);
     });
 
-    // Generates a new value.
-    const generate = async () => {
+    // Sends the payload and prepares the relevant response.
+    const execute = async (widgetId: EnumWidgetId, payload?: IGenericExecuteBatchRequestItemPayload) => {
         // Prepare a new batchId
         const batchId = uuidv4();
+        const itemId = uuidv4();
 
         setLoading(true);
 
@@ -31,8 +32,9 @@ export function WidgetTest() {
             batchId: batchId,
             requestItems: [
                 {
-                    itemId: "456",
-                    widgetId: WidgetIds.AUSTRALIAN_COMPANY_NUMBER_GENERATOR,
+                    itemId: itemId,
+                    widgetId: widgetId,
+                    payload,
                 },
             ],
         }));
@@ -42,18 +44,46 @@ export function WidgetTest() {
         setLoading(false);
     }
 
+    // Generate
+    const generate = async () => {
+        await execute(EnumWidgetId.AUSTRALIAN_COMPANY_NUMBER_GENERATOR);
+    }
+
+    // Validate
+    const validate = async () => {
+        const payload = {
+            acn: currentInputValue,
+        };
+
+        await execute(EnumWidgetId.AUSTRALIAN_COMPANY_NUMBER_VALIDATOR, payload);
+    }
+
+    const onValueChangeHandler = async (e: any) => {
+        setCurrentInputValue(e.target.value);
+    }
+
     return (
         <div>
             {loading ? "Loading" : (
-                <button
-                    aria-label="Increment value"
-                    onClick={generate}
-                >
-                    +
-                </button>
+                <>
+                    <button
+                        aria-label="Increment value"
+                        onClick={generate}
+                    >
+                        Generate
+                    </button>
+                    <input type="text" onChange={onValueChangeHandler} />
+                    <button
+                        aria-label="Increment value"
+                        onClick={validate}
+                    >
+                        Validate
+                    </button>
+                </>
             )}
 
             <div>Batch ID: {currentBatchId}</div>
+            <div>Current Value: {currentInputValue}</div>
             <pre>
                 {JSON.stringify(batchItems, null, 4)}
             </pre>
