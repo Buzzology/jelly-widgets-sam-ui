af0edde3765d7f62bb73aad8d00aa7554780b550
Buzzology <chrisowens321@gmail.com>
2022-01-19 18:55:59 +1000

    [CHORE] Additional api logging (temp)

--- src/components/widgetExecutions/widgetExecutionsApi.ts
+++ src/components/widgetExecutions/widgetExecutionsApi.ts
@@ -1,15 +1,20 @@
 import { WidgetExecuteBatchRequest, WidgetExecuteBatchResponse } from "../../@types/WidgetExecuteBatch";
 
 export async function WidgetRequestsExecuteBatch(request: WidgetExecuteBatchRequest): Promise<WidgetExecuteBatchResponse> {
-  const resp = await fetch(`${process.env.REACT_APP_BASE_API_URL}/execute-batch`, {
+  try {
+    const resp = await fetch(`${process.env.REACT_APP_BASE_API_URL}/execute-batch`, {
       method: 'POST',
       // headers: headers,
       body: JSON.stringify(request),
     });
 
-    if(!resp.ok){
-        throw new Error(resp.statusText);
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
     } else {
-        return await resp.json() as WidgetExecuteBatchResponse;
+      return await resp.json() as WidgetExecuteBatchResponse;
     }
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
+}
