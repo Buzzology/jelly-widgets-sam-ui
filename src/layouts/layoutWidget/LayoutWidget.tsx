38f8b6c98dc36cc3142835fa438d8ead0f50220e
Buzzology <chrisowens321@gmail.com>
2022-01-21 07:15:29 +1000

    [UX] Fix layout for widgets

--- src/layouts/layoutWidget/LayoutWidget.tsx
+++ src/layouts/layoutWidget/LayoutWidget.tsx
@@ -5,8 +5,6 @@ import './layoutWidget.css';
 type LayoutWigdetProps = {
     children: React.ReactNode;
     loading: boolean;
-    title: string,
-    description: string,
     headerSlug: string,
     headerTitle: string,
     headerDescription: string,
@@ -17,8 +15,6 @@ type LayoutWigdetProps = {
 const LayoutWidget = ({
     children,
     loading,
-    title,
-    description,
     headerSlug,
     headerTitle,
     headerDescription,
@@ -30,7 +26,7 @@ const LayoutWidget = ({
             <div className="flex justify-center bg-sky-600 text-white p-4 pb-10" style={{ position: 'relative' }}>
                 <div id="widget-header-background"></div>
                 <div className="max-w-md md:max-w-5xl flex-grow grid grid-cols-12">
-                    <div className="col-span-12 md:col-span-6" style={{zIndex: 2}}>
+                    <div className="col-span-12 md:col-span-6" style={{ zIndex: 2 }}>
                         <h1 className="text-sm uppercase leading-normal mt-8 font-light text-sky-200">
                             {headerSlug}
                         </h1>
@@ -62,17 +58,9 @@ const LayoutWidget = ({
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-12">
-                <div className="col-span-12 mt-4 pb-4">
-                    {title}
-                </div>
-                <p className="text-sm text-gray-500 font-light">
-                    {description}
-                </p>
-                <div className="col-span-12">
-                    <div>
-                        {children}
-                    </div>
+            <div className="min-h-screen flex justify-center">
+                <div className="max-w-md md:max-w-5xl flex-grow pt-10">
+                    {children}
                 </div>
             </div>
         </LayoutDefault>
