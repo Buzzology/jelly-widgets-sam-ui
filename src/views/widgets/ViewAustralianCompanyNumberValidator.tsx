02c387ac5a0fffac757b1b21070ebc39aca503be
Buzzology <chrisowens321@gmail.com>
2022-01-21 07:16:03 +1000

    [CHORE] Remove unused properties for acn validator

--- src/views/widgets/ViewAustralianCompanyNumberValidator.tsx
+++ src/views/widgets/ViewAustralianCompanyNumberValidator.tsx
@@ -6,8 +6,6 @@ import LayoutWidget from '../../layouts/layoutWidget/LayoutWidget';
 export default function ViewAustralianCompanyNumberValidator() {
     return (
         <LayoutWidget
-            title="Australian Company Number Validator"
-            description="Validate Australian Company Numbers"
             loading={false}
             headerSlug="widgets"
             headerTitle="Acn Validator"
