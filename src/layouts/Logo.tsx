a98de5fba005daa9ae21846efa79f3a33ebe3d2d
Buzzology <chrisowens321@gmail.com>
2022-01-24 18:15:18 +1000

    [CHPRE] Rename jellywidgets to testerwidgets

--- src/layouts/Logo.tsx
+++ src/layouts/Logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({color }: { color: string}) {
     return (
         <header className="header">
             <div className="container logo pointer" style={{color: color || "#333"}}>
-                <Link to="/"><span className="tracking-tight">JellyWidgets</span></Link>
+                <Link to="/"><span className="tracking-tight">TesterWidgets</span></Link>
             </div>
         </header>
     );
