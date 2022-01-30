df3aff899349912f7201aa5a845028abce0b9f18
Buzzology <chrisowens321@gmail.com>
2022-01-31 08:35:36 +1000

    [FIX] Ensure that ga path is correct

--- src/routes/Hooks.ts
+++ src/routes/Hooks.ts
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 // Used to change the document's title via a hoook.
 const useDocTitle = (title: string) => {
     const [doctitle, setDocTitle] = useState(title);
+    const { pathname } = useLocation();    
 
     useEffect(() => {
-        document.title = doctitle;
-    }, [doctitle]);
+        // Change the document title.
+        setDocTitle(doctitle);
+
+        // Report a page change to ga.
+        (window as any).gtag('event', 'page_view', {
+            page_title: doctitle,
+            page_path: pathname,
+            send_to: 'G-FEE62SDTH9'
+          });
+    }, [doctitle, pathname]);
 
     return [doctitle, setDocTitle];
 };
