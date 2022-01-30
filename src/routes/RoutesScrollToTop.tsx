fb6b11f25d45b0710fad38f11e66c92ad5adc5c7
Buzzology <chrisowens321@gmail.com>
2022-01-30 20:56:07 +1000

    [FIX] Include ga in scrolltotop so that it is within react-router

--- src/routes/RoutesScrollToTop.tsx
+++ src/routes/RoutesScrollToTop.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import ReactGA from 'react-ga';
 
 // https://stackoverflow.com/a/64363845/522859
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
     useEffect(() => {
+        ReactGA.pageview(pathname);
         window.scrollTo(0, 0);
     }, [pathname]);
 
