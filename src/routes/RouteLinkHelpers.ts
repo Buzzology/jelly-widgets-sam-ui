11589480d9f4efbae854cb9e876ec8138ce67971
Buzzology <chrisowens321@gmail.com>
2022-01-31 11:51:30 +1000

    [BUG] Fix tfn validator link

--- src/routes/RouteLinkHelpers.ts
+++ src/routes/RouteLinkHelpers.ts
@@ -7,5 +7,5 @@ export const GetContactPageLink = () => '/contact';
 // Widget links.
 export const GetAcnValidatorLink = () => `/widgets/${EnumWidgetId.AUSTRALIAN_COMPANY_NUMBER_VALIDATOR}/australian-company-number-validator`;
 export const GetAcnGeneratorLink = () => `/widgets/${EnumWidgetId.AUSTRALIAN_COMPANY_NUMBER_GENERATOR}/australian-company-number-generator`;
-export const GetTfnValidatorLink = () => `/widgets/${EnumWidgetId.AUSTRALIAN_TAX_FILE_NUMBER_VALIDATOR}/australian-company-number-validator`;
+export const GetTfnValidatorLink = () => `/widgets/${EnumWidgetId.AUSTRALIAN_TAX_FILE_NUMBER_VALIDATOR}/australian-tax-file-number-validator`;
 export const GetTfnGeneratorLink = () => `/widgets/${EnumWidgetId.AUSTRALIAN_TAX_FILE_NUMBER_GENERATOR}/australian-tax-file-number-generator`;
