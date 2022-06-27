9c32140ff508009b2576cfe7a1158a61167948e0
Buzzology <chrisowens321@gmail.com>
2022-06-27 13:08:35 +1000

    [ADD] Include ads.txt

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,5 +1,9 @@
 /* https://stackoverflow.com/a/7224605/522859 */
 export function CapitalizeFirstCharacter(s: string)
 {
-    return s && s[0].toUpperCase() + s.slice(1);
+    if(!s.length) return s;
+
+    if(s.length === 1) return s.toUpperCase();
+
+    return s[0].toUpperCase() + s.slice(1);
 }
